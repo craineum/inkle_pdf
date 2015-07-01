b3fd5299367b4b78f16c9396fc25d238114f24e6
Kevin Craine <craineum@gmail.com>
2015-07-01 10:18:52 -0400

    Allow for void tags

--- lib/markup_converter.rb
+++ lib/markup_converter.rb
@@ -8,25 +8,37 @@ class MarkupConverter
   def tags(string)
     maps.inject(string) do |text, map|
       @map = map
-      text.gsub(/#{swap_start}(.*?)#{swap_end}/, "#{tag_start}\\1#{tag_end}")
+      if map.has_key? :from
+        text.gsub(/#{swap_void}/, "#{tag_void}")
+      else
+        text.gsub(/#{swap_start}(.*?)#{swap_end}/, "#{tag_start}\\1#{tag_end}")
+      end
     end
   end
 
   private
 
+  def swap_void
+    map[:from]
+  end
+
+  def tag_void
+    "<#{map[:to]} />"
+  end
+
   def swap_start
-    map[:from] || map[:from_start]
+    map[:from_start]
   end
 
   def swap_end
-    map[:from] || map[:from_end]
+    map[:from_end]
   end
 
   def tag_start
-    "<#{map[:to] || map[:to_start]}>"
+    "<#{map[:to]}>"
   end
 
   def tag_end
-    "</#{map[:to] || map[:to_end]}>"
+    "</#{map[:to]}>"
   end
 end
--- test/lib/markup_converter_test.rb
+++ test/lib/markup_converter_test.rb
@@ -2,27 +2,37 @@ require 'test_helper'
 
 class MarkupConverterTest < Minitest::Test
 
-  def test_tags
+  def test_void_tag
     maps = [{ from: /-/, to: 'a' }]
     converter = MarkupConverter.new maps
-    assert_equal 'code <a>monkey</a>', converter.tags('code -monkey-')
+    assert_equal 'code <a /> monkey', converter.tags('code - monkey')
   end
 
-  def test_tags_different_start_end
-    maps = [{ from_start: /\*-/, from_end: /-\*/, to_start: 'a', to_end: 'b' }]
-    converter = MarkupConverter.new maps
-    assert_equal 'code <a>monkey</b>', converter.tags('code *-monkey-*')
-  end
-
-  def test_tags_multiple_same_map
+  def test_void_tag_multiple
     maps = [{ from: /-/, to: 'a' }]
     converter = MarkupConverter.new maps
-    assert_equal '<a>code</a> <a>monkey</a>', converter.tags('-code- -monkey-')
+    assert_equal 'code <a /> <a /> monkey', converter.tags('code - - monkey')
   end
 
-  def test_tags_multiple_different_map
-    maps = [{ from: /-/, to: 'a' }, { from: /_/, to: 'b' }]
+  def test_tag
+    maps = [{ from_start: /\*-/, from_end: /-\*/, to: 'a' }]
     converter = MarkupConverter.new maps
-    assert_equal '<a>code</a> <b>monkey</b>', converter.tags('-code- _monkey_')
+    assert_equal 'code <a>monkey</a>', converter.tags('code *-monkey-*')
+  end
+
+  def test_tag_multiple
+    maps = [{ from_start: /\*-/, from_end: /-\*/, to: 'a' }]
+    converter = MarkupConverter.new maps
+    assert_equal '<a>code</a> <a>monkey</a>',
+      converter.tags('*-code-* *-monkey-*')
+  end
+
+  def test_tags_multiple
+    maps = [{ from_start: /\*-/, from_end: /-\*/, to: 'a' },
+            { from_start: /\*=/, from_end: /=\*/, to: 'b' },
+            { from: /-/, to: 'c' }]
+    converter = MarkupConverter.new maps
+    assert_equal '<a>code</a> <c /> <b>monkey</b>',
+      converter.tags('*-code-* - *=monkey=*')
   end
 end
